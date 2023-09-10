c48e01a8e49e1f2e4fec0ed34894d2f5481ddc64
vasanthakumaaran <vasanthakumaaran76@gmail.com>
2023-09-10 14:50:56 +0400

    removed

--- Client/client-app/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export {default as Home} from "./Home";
-export {default as profile} from "./CampaignDetails";
-export {default as createCampaign} from "./CreateCampaign";
-export {default as CampaignDetails} from "./CampaignDetails";
