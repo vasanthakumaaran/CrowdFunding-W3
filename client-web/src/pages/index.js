6a38db9b78cf4e0def5a01bc4c252e2520d61dc6
vasanthakumaaran <vasanthakumaaran76@gmail.com>
2023-10-13 14:20:30 +0400

    final version

--- client-web/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export {default as Home} from "./Home"
-export {default as CreateCampaign} from "./CreateCampaign"
-export {default as CampaignDetails} from "./CampaignDetails"
-export {default as Profile} from "./Profile"
